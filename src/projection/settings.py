d6abf34adc861f8be9cc8191e9f268a4befff334
Syed Abdullah <syed@syedabdullah.net>
2016-06-25 23:59:27 +0800

    Added base static directory to STATICFILES_DIR

--- src/projection/settings.py
+++ src/projection/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Test Runner Configuration
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
