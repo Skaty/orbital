f5fc533a4949d9bb2bcc18e7b0cc8cfdbc199b97
Syed Abdullah <syed@syedabdullah.net>
2016-06-26 01:17:07 +0800

    Added new URL routes for the dashboard

--- src/projection/urls.py
+++ src/projection/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^projects/', include('projects.urls', namespace='projects')),
+    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
+    url(r'^auth/', include('django.contrib.auth.urls'))
 ]
