ef7eb95cd63284ed8ff89056177d2055a0d5c3af
Syed Abdullah <syed@syedabdullah.net>
2016-06-26 18:05:31 +0800

    Added MESSAGE_TAGS setting for error messages, so that it follow Bootstrap's class naming convention

--- src/projection/settings.py
+++ src/projection/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as message_constants
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,4 +138,10 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 LOGIN_URL = '/auth/login/'
 
 LOGIN_REDIRECT_URL = '/'
+
+# Messaging
+
+MESSAGE_TAGS = {
+    message_constants.ERROR: 'danger'
+}
 
