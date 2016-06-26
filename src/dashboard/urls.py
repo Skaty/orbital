63795dee9e3f4645385723be32b9ea6e9560fae3
Syed Abdullah <syed@syedabdullah.net>
2016-06-26 17:26:17 +0800

    Added routing and link for target setting

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url, include
 from dashboard import views
 
 urlpatterns = [
-    url(r'^(?P<project_id>[0-9]+)/$', views.home_view, name='home'),
+    url(r'^(?P<project_id>[0-9]+)/', include([
+        url(r'^$', views.home_view, name='home'),
+        url(r'^achievements/', include('targets.urls', namespace='achievements'))
+    ])),
 ]
