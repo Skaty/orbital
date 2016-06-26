97457cdaeac69637ec5a4e7c48703408e46bc190
Syed Abdullah <syed@syedabdullah.net>
2016-06-26 17:24:55 +0800

    Added UI for groups to add their targets

--- src/targets/urls.py
+++ src/targets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 
+from targets.views import TargetGoalCreateView
 
 urlpatterns = [
-
+    url(r'^targets/add/$', TargetGoalCreateView.as_view(), name="add-group-target"),
 ]
--- src/targets/views.py
+++ src/targets/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 
-# Create your views here.
+from targets.models import Target
+
+
+class TargetGoalCreateView(CreateView):
+    template_name_suffix = '_group_create_form'
+    model = Target
+    fields = ['name', 'description', 'deadline']
+
+    def dispatch(self, request, *args, **kwargs):
+        project_id = self.kwargs.get('project_id')
+        try:
+            self.projectgroup = request.user.projectgroup_set.get(project_id=project_id)
+        except ObjectDoesNotExist:
+            return redirect('/')
+        return super(TargetGoalCreateView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Adds projectgroup to template context
+        """
+        kwargs['projectgroup'] = self.projectgroup
+        return super(TargetGoalCreateView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        form.instance.group = self.projectgroup
+        form.instance.created_by = self.request.user
+        return super(TargetGoalCreateView, self).form_valid(form)
