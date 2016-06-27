219fadea8f3db24f0f2360c5bf76f1f1745b2607
Syed Abdullah <syed@syedabdullah.net>
2016-06-27 22:30:11 +0800

    Added Project Creation UI logic

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from projects.views import ProjectListView, ProjectDetailView, ProjectGroupCreateView
+from projects.views import ProjectListView, ProjectCreateView, ProjectDetailView, ProjectGroupCreateView
 
 urlpatterns = [
     url(r'^$', ProjectListView.as_view(), name='project-list'),
+    url(r'^create/$', ProjectCreateView.as_view(), name='project-create'),
     url(r'^(?P<pk>[0-9]+)/$', ProjectDetailView.as_view(), name='project-detail'),
     url(r'^(?P<project_pk>[0-9]+)/groups/add/$', ProjectGroupCreateView.as_view(), name='group-create')
 ]
--- src/projects/views.py
+++ src/projects/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse_lazy
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, DetailView
@@ -12,6 +13,25 @@ from projects.models import Project, ProjectGroup
 @method_decorator(login_required, name='dispatch')
 class ProjectListView(ListView):
     model = Project
+
+
+@method_decorator(login_required, name='dispatch')
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = ['name', 'facilitators', 'ends_on']
+    success_url = reverse_lazy('projects:project-list')
+
+    def get_form(self, form_class=None):
+        form = super(ProjectCreateView, self).get_form(form_class)
+        form.initial['facilitators'] = [self.request.user]
+        return form
+
+    def form_valid(self, form):
+        self.object = form.save()
+        if self.request.user not in self.object.facilitators.all():
+            self.object.facilitators.add(self.request.user)
+        return HttpResponseRedirect(reverse_lazy('projects:project-detail', kwargs={'pk': self.object.pk}))
+
 
 @method_decorator(login_required, name='dispatch')
 class ProjectDetailView(DetailView):
@@ -48,3 +68,4 @@ class ProjectGroupCreateView(CreateView):
     def form_valid(self, form):
         form.instance.project = self.project
         return super(ProjectGroupCreateView, self).form_valid(form)
+
