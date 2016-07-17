905d13516049c4d5dcf055d90a1d65e1c6558642
Syed Abdullah <syed@syedabdullah.net>
2016-07-17 11:53:56 +0800

    Added post_save signal to TargetAssignment so that the actual Target can be marked as complete if all assignees mark the target as completed

--- projection/settings.py
+++ projection/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nose',
-    'projects',
-    'targets',
-    'miscellaneous',
+    'projects.apps.ProjectsConfig',
+    'targets.apps.TargetsConfig',
+    'miscellaneous.apps.MiscellaneousConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
