e19966f63a77a2f3d57602445d3d450101ea2504
Syed Abdullah <syed@syedabdullah.net>
2016-07-23 13:09:57 +0800

    Created Preferences model for User

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from pytz import common_timezones
 
 # Create your models here.
+class Preferences(models.Model):
+    TZ_CHOICES = [(x, x.replace('_', ' ')) for x in common_timezones]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, on_delete=models.CASCADE)
+    timezone = models.CharField(
+        max_length=255,
+        choices=TZ_CHOICES,
+        default='UTC'
+    )
