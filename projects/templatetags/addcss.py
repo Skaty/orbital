ea5beca0ce368cddc3e9eb1c6468565084ace74a
Syed Abdullah <syed@syedabdullah.net>
2016-07-26 01:37:09 +0800

    Add addcss file, that will append the magic class to any fields with DateTime capabilities.

--- projects/templatetags/addcss.py
+++ projects/templatetags/addcss.py
@@ -1,7 +1,12 @@
 from django import template
+from django.forms import fields
+
 register = template.Library()
 
 
 @register.filter(name='addcss')
 def addcss(field, css):
-   return field.as_widget(attrs={"class":css})
+   attributes = {'class': css}
+   if isinstance(field.field, fields.DateTimeField):
+      attributes['class'] += ' bs-datetime'
+   return field.as_widget(attrs=attributes)
